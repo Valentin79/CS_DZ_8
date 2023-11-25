f3ec40eff2a12009e5b4d9074260c5c8f492e756
Valentin <99755643+Valentin79@users.noreply.github.com>
2023-11-25 23:11:20 +0300

    dz_8

---
+++ DZ_Cem_8.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CS_Sem_8
+{
+    internal class DZ_Cem_8
+    {
+        public static void FindSomeFile(string path, string extension, string text)
+        {
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Не найдено");
+                return;
+                
+            }
+
+            var dirs = Directory.EnumerateDirectories(path);
+            var files = Directory.EnumerateFiles(path);
+                foreach (var file in files)
+                {
+                    if (Path.GetExtension(file).Equals($".{extension}")) // точку добавил в ручную, т.к. Path.GetExtension(file) выдает расширение с ней
+                    {
+                        Console.WriteLine(file);
+                        //============Поиск текста===============
+                        using (StreamReader sr = new StreamReader(file))
+                        {
+                            while (!sr.EndOfStream)
+                            {
+                                string value = sr.ReadLine();
+                                if (value.Contains(text))
+                                {
+                                    Console.WriteLine($"Файл: {file} содержит текст: {value}");
+                                    break;
+                                }
+                            }
+                        }
+                        //========================================
+
+                    }
+                    
+                }
+                foreach (var dir in dirs)
+                {
+                    FindSomeFile(dir, extension, text);
+                }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace CS_Sem_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CopyFileMetods.MyCopyFile(@"C:\Users\Valentin\Desktop\C#\CS_Sem_8\CS_Sem_8\Program.cs", "test.txt");
+            //CopyFileMetods.CopyFile(@"C:\Users\Valentin\Desktop\C#\CS_Sem_8\CS_Sem_8\Program.cs", "test2.txt"); // запишет в рабочую директорию(где exe)
+
+            //FindFileMetod.FindSomething("D:\\Разное\\Pony", "Pony");
+
+            //SomeReadMetod.ReadSomething(@"D:\Разное\Pony\Пони.txt");
+
+            //ChangeFilsMetod.ChangeSomeFile(@"D:\Разное\Pony\Pony_test.txt");
+
+            DZ_Cem_8.FindSomeFile(@"D:\Разное\Test\", "txt", "Test");
+        }
+
+
+
+    }
+}
